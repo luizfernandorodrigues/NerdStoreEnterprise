790b59b5301d35b38ff888af6b26fbaf4dd54ff0
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2021-02-12 01:01:40 -0300

    Inicio da implementação da fila de mensagem de comunicação entre as API's

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using EasyNetQ;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using NSE.Core.Messages.Integration;
 using NSE.Identidade.API.Models;
 using NSE.WebAPI.Core.Controllers;
 using NSE.WebAPI.Core.Identidade;
@@ -24,6 +26,7 @@ namespace NSE.Identidade.API.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly AppSettings _appSettings;
+        private IBus _bus;
 
         #endregion
 
@@ -56,7 +59,11 @@ namespace NSE.Identidade.API.Controllers
             var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
 
             if (result.Succeeded)
+            {
+                var sucesso = await RegistrarCliente(usuarioRegistro);
+
                 return CustomResponse(await GerarJwt(usuarioRegistro.Email));
+            }
 
             foreach (var error in result.Errors)
                 AdicionarErroProcessamento(error.Description);
@@ -90,6 +97,20 @@ namespace NSE.Identidade.API.Controllers
         #endregion
 
         #region Métodos Privados
+
+        private async Task<ResponseMessage> RegistrarCliente(UsuarioRegistro usuarioRegistro)
+        {
+            var usuario = await _userManager.FindByEmailAsync(usuarioRegistro.Email);
+
+            var usuarioRegistrado = new UsuarioRegistradoIntegrationEvent(Guid.Parse(usuario.Id), usuarioRegistro.Nome, usuarioRegistro.Email, usuarioRegistro.Cpf);
+
+            _bus = RabbitHutch.CreateBus("host=localhost:5672");
+
+            var sucesso = await _bus.RequestAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(usuarioRegistrado);
+
+            return sucesso;
+        }
+
 
         private async Task<UsuarioRespostaLogin> GerarJwt(string email)
         {
